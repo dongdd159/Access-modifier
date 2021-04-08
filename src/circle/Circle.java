1f0c8e6566df2eb3f659ce1386ce71e5f4ac1a78
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-04-08 22:16:55 +0700

    accessmodify

---
+++ src/circle/Circle.java
@@ -0,0 +1,20 @@
+package circle;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    Circle() {
+    }
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea() {
+        return Math.PI *radius ;
+    }
+}
